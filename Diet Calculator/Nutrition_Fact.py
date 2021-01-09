0666d5dc82a131e3e314f8c192a5b5bd4742f28a
dennisyakovlev <dennis.yakovlev@yahoo.ca>
2021-01-09 16:31:41 -0500

    changed nutrition

--- Diet Calculator/Nutrition_Fact.py
+++ Diet Calculator/Nutrition_Fact.py
@@ -34,8 +34,6 @@ class Nutrition_Fact(E_Base.Element_Base):
     #return list of values containing 8 info facts
     def __mul__(self, number):
 
-        #values = [print(item) for item in self.values]
-
         values = [int(float(item) * float(number)) for item in self.values]
 
         return values
@@ -82,4 +80,38 @@ class Nutrition_Fact(E_Base.Element_Base):
 
 class Temp(E_Base.Element_Base):
 
-    pass
+    #name - name of nutrition fact
+    #values - length of 10 numbers for the nutritional info
+    #total fat and total carbohydrates are not entered SHOULD BE CHANGED to accept total fat and carbs
+
+    #adds two nutrition facts
+    #returns list of values containing 8 info facts
+    def __add__(self, other):
+
+        toRet = []
+        for i in range(0, len(self.values)):
+            toRet.append(self.values[i] + other.values[i])
+        return toRet
+
+    #n1 - n2
+    #subtracts n2 FROM n1
+    #returns list of values containing 8 info facts
+    #return [] if one of values is negative
+    def __sub__(self, other):
+
+        toRet = []
+        for i in range(0, len(self.values)):
+            val = self.values[i] - other.values[i]
+            if val < 0:
+                return []
+            toRet.append(val)
+        return toRet
+
+    #nurition_Fact * number
+    #multiplies all the 8 values by number
+    #return list of values containing 8 info facts
+    def __mul__(self, number):
+
+        values = [int(float(item) * float(number)) for item in self.values]
+
+        return values
